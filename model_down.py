f8e380b63834602d52db503c28fa62a4e10b1b81
H Wayment-Steele <hannah.wayment.steele@gmail.com>
2023-04-11 15:23:53 -0400

    Update model_down.py

--- model_down.py
+++ model_down.py
@@ -17,14 +17,6 @@ def _init_weights(module):
     elif isinstance(module, nn.Linear) and module.bias is not None:
         module.bias.data.zero_()
 
-
-def accuracy(logits, labels, ignore_index: int = -100):
-    with torch.no_grad():
-        valid_mask = (labels != ignore_index)
-        predictions = logits.float().argmax(-1)
-        correct = (predictions == labels) * valid_mask
-        return correct.sum().float() / valid_mask.sum().float()
-
 class SimpleMLP(nn.Module):
 
     def __init__(self,
@@ -64,11 +56,20 @@ class SimpleConv(nn.Module):
         x = x.transpose(1, 2).contiguous()
         return x
 
-def accuracy(logits, labels, ignore_index: int = -100):
+# def accuracy(logits, labels, ignore_index: int = -100):
+#     with torch.no_grad():
+#         valid_mask = (labels != ignore_index)
+#         predictions = logits.float().argmax(-1)
+#         correct = (predictions == labels) * valid_mask
+#         return correct.sum().float() / valid_mask.sum().float()
+
+def dyn_accuracy(logits, labels): #, dyn_label: int = 3):
     with torch.no_grad():
+        # just care about assignments 'static' 2 or 'dynamic' 3
+        valid_mask = torch.logical_or(labels == 2, labels==3)
         predictions = logits.float().argmax(-1)
-        correct = (predictions == labels)# * valid_mask
-        return correct.sum().float() / logits.shape[0]#valid_mask.sum().float()
+        correct = (predictions == labels) * valid_mask
+        return correct.sum().float() / valid_mask.sum().float()
 
 class Accuracy(nn.Module):
 
@@ -77,14 +78,17 @@ class Accuracy(nn.Module):
         self.ignore_index = ignore_index
 
     def forward(self, inputs, target):
-        return accuracy(inputs, target, self.ignore_index)
-    
+        return dyn_accuracy(inputs, target) #,  self.ignore_index)
+
+weights = [0.1,0.1, 0.1, 1.0]
+class_weights = torch.FloatTensor(weights).cuda()
+
 class SequenceToSequenceClassificationHead(nn.Module):
 
     def __init__(self,
                  hidden_size: int,
                  num_labels: int,
-                 ignore_index: int = -100):
+                 ignore_index: int = 0):
         super().__init__()
         self.classify = SimpleConv(hidden_size, 1280, num_labels)
         self.num_labels = num_labels
@@ -94,7 +98,7 @@ class SequenceToSequenceClassificationHead(nn.Module):
         sequence_logits = self.classify(sequence_output)
         outputs = (sequence_logits,)
         if targets is not None:
-            loss_fct = nn.CrossEntropyLoss(ignore_index=self._ignore_index)
+            loss_fct = nn.CrossEntropyLoss(weight = class_weights,ignore_index=self._ignore_index)
             classification_loss = loss_fct(
                 sequence_logits.view(-1, self.num_labels), targets.view(-1))
             acc_fct = Accuracy(ignore_index=self._ignore_index)
@@ -108,7 +112,7 @@ class ProteinBertForSequence2Sequence(nn.Module):
 
     def __init__(self):
         super().__init__()
-        self.num_labels = 10 # change for dynamics classification
+        self.num_labels = 4 # changed from 10 for dynamics classification
         self.bert = model
         self.classify = SequenceToSequenceClassificationHead(
             model.embed_dim, self.num_labels)
@@ -122,7 +126,6 @@ class ProteinBertForSequence2Sequence(nn.Module):
                 v.requires_grad = False
             elif not finetune_emb and 'embed_positions.weight' in k:
                 v.requires_grad = False
-
 
         outputs = self.bert(input_ids, repr_layers=[6])
         sequence_output = outputs['representations'][6]
